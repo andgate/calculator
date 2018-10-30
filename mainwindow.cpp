199e60691cd3aefd9c926cc4d1e738465974982c
Gabe <gabe4k@gmail.com>
2018-10-30 08:44:09 -0700

    Took off some unneccessary buttons. Slimmed the calculator down.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -31,7 +31,7 @@ void MainWindow::on_solveButton_clicked()
 
 void MainWindow::on_n7_clicked()
 {
-    ui->inputBox->setText(ui->inputBox->text()+"7");
+    ui->inputBox->insert("7");
 }
 
 void MainWindow::on_inputBox_returnPressed()
@@ -48,4 +48,89 @@ void MainWindow::on_clearButton_clicked()
 {
     ui->historyBox->undo();
     ui->inputBox->setText("");
+}
+
+void MainWindow::on_n0_clicked()
+{
+    ui->inputBox->insert("0");
+}
+
+void MainWindow::on_n1_clicked()
+{
+    ui->inputBox->insert("1");
+}
+
+void MainWindow::on_n2_clicked()
+{
+    ui->inputBox->insert("2");
+}
+
+void MainWindow::on_n3_clicked()
+{
+    ui->inputBox->insert("3");
+}
+
+void MainWindow::on_addButton_clicked()
+{
+    ui->inputBox->insert("+");
+}
+
+void MainWindow::on_subButton_clicked()
+{
+    ui->inputBox->insert("-");
+}
+
+void MainWindow::on_mulButton_clicked()
+{
+    ui->inputBox->insert("*");
+}
+
+void MainWindow::on_divButton_clicked()
+{
+    ui->inputBox->insert("/");
+}
+
+void MainWindow::on_powButton_clicked()
+{
+    ui->inputBox->insert("^");
+}
+
+void MainWindow::on_decimalButton_clicked()
+{
+    ui->inputBox->insert(".");
+}
+
+void MainWindow::on_lparen_clicked()
+{
+    ui->inputBox->insert("(");
+}
+
+void MainWindow::on_rparen_clicked()
+{
+    ui->inputBox->insert(")");
+}
+
+void MainWindow::on_n4_clicked()
+{
+    ui->inputBox->insert("4");
+}
+
+void MainWindow::on_n5_clicked()
+{
+    ui->inputBox->insert("5");
+}
+
+void MainWindow::on_n6_clicked()
+{
+    ui->inputBox->insert("6");
+}
+
+void MainWindow::on_n8_clicked()
+{
+    ui->inputBox->insert("8");
+}
+
+void MainWindow::on_n9_clicked()
+{
+    ui->inputBox->insert("9");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -25,6 +25,40 @@ private slots:
 
     void on_clearButton_clicked();
 
+    void on_n0_clicked();
+
+    void on_n1_clicked();
+
+    void on_n2_clicked();
+
+    void on_n3_clicked();
+
+    void on_addButton_clicked();
+
+    void on_subButton_clicked();
+
+    void on_mulButton_clicked();
+
+    void on_divButton_clicked();
+
+    void on_powButton_clicked();
+
+    void on_decimalButton_clicked();
+
+    void on_lparen_clicked();
+
+    void on_rparen_clicked();
+
+    void on_n4_clicked();
+
+    void on_n5_clicked();
+
+    void on_n6_clicked();
+
+    void on_n8_clicked();
+
+    void on_n9_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
